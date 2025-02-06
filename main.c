063fcd104bbf21ac9ea451d24a1b310623011033
manasg1111 <118659036+manasg1111@users.noreply.github.com>
2025-02-06 10:14:39 +0530

    💡 Added comments for the malloc function

--- main.c
+++ main.c
@@ -22,44 +22,61 @@ int allocated_or_freed_mapping[ALLOCATED_CHUNKS_CAPACITY] = {0};   //allocated=1
 
 
 void* my_alloc(size_t size){
+  // Check if the size of the heap exceeds the maximum size
   if(heap_size + size > HEAP_CAPACITY) {
+    // If there is no space at the end, go through allocated bitmap and look for a freed block with required size
     for(int i = 0; i <= allocated_chunks_size; i++){
+      // Check if freed and if chunk size is bigger than required
       if(!allocated_or_freed_mapping[i] && chunks[i].size >= size) {
         void* result;
+        // If requirement matched, allocate buffer mark as allocated
         allocated_or_freed_mapping[i] = 1;
         if(chunks[i].size == size) {
+          // If no resizing requried, return the pointer to the start
           result = chunks[i].start;
         } else {
+          // If required size is lesser, resize the chunk and return only the necessary amount of memory
           result = chunks[i].start;
           const CHUNK allocated_chunk = {
             .start = chunks[i].start + size,
             .size = chunks[i].size - size,
           };
+          // New chunk of (total size - required size) added
           chunks[allocated_chunks_size] = allocated_chunk;
           allocated_or_freed_mapping[allocated_chunks_size++] = 0;
           chunks[i].size = size;
         }
-      return result;
+        // Return pointer
+        return result;
       }
     }
+    // If not enough space and no block available, return memory exceeded
     printf("heap memory exceeded!");
     exit(1);
   }
+
+  // If size available at the end, just return end pointer and add a new allocated chunk to allocated buffer
+  // Add the new chunk size to total size
   void* result = heap + heap_size;
   heap_size += size;
 
+  // If chunk number limit exceeded return error
   if(allocated_chunks_size > ALLOCATED_CHUNKS_CAPACITY) {
     printf("chunk size exceeded");
     exit(1);
   }
+
+  // New chunk
   const CHUNK allocated_chunk = {
     .start = result,
     .size = size,
   };
   
+  // Add the new chunk to the allocated buffer
   chunks[allocated_chunks_size] = allocated_chunk;
   allocated_or_freed_mapping[allocated_chunks_size++] = 1;
 
+  // Return the pointer
   return result;
 }
 
